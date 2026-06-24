d4bada7710706f7f737ded86fb31f1311dfd4a4e
Rann Day <5001592+rannday@users.noreply.github.com>
2026-06-23 22:00:22 -0400

    Refactor Bitburner API for embedding

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 pub mod args;
+pub mod cli;
 pub mod error;
 pub mod fs_sync;
 pub mod path;
 pub mod remote;
 pub mod ws;
 
-pub mod cli;
+pub use remote::{
+    BitburnerApi, BitburnerFile, DEFAULT_ADDRESS, DEFAULT_REQUEST_TIMEOUT, DEFAULT_SERVER,
+    FileMetadata, JsonRpcError, JsonRpcRequest, JsonRpcResponse, RemoteClient, SaveFile,
+    ServerInfo,
+};
